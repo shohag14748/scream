1e615eb455e09ba69f1ea29b970d33927e82841d
Mohammad Ali Shohag <47399558+shohag14748@users.noreply.github.com>
2020-06-01 15:31:15 +0600

    upload

---
+++ js/scripts.js
@@ -0,0 +1,256 @@
+new WOW().init();
+
+
+
+
+
+"use strict";
+
+
+
+$(".parallax_image").each(function() {
+
+    var attr = $(this).attr('data-image-src');
+
+    if (typeof attr !== typeof undefined && attr !== false) {
+
+        $(this).css('background-image', 'url(' + attr + ')');
+
+        //$(this).css('background-size', 'cover');
+
+        $(this).css('background-position', 'center');
+
+        $(this).css('background-repeat', 'no-repeat');
+
+    }
+
+});
+
+
+
+$(window).on('load', function() {
+
+        if ($('.spinner-preloader-wrap').length) {
+
+            $('.spinner-preloader-wrap').fadeOut(500);
+
+        }
+
+        if ($('.loader__svg').length) {
+
+            setTimeout(stopPreloader, 900);
+
+        }        
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+    if ($("#nav-transparent").length !== 0) {
+
+        if ($(window).width() > 991) {
+
+            $("#nav-transparent #main_logo").css("display", "none");
+
+        } else {
+
+            $("#nav-transparent #light_logo").css("display", "none");
+
+        }
+
+        $(window).scroll(function() {
+
+            var scroll = $(window).scrollTop();
+
+            if ($(window).width() > 991) {
+
+                if (scroll > 30) {
+
+                    $(".navigation-fixed-wrapper").addClass("nav-white-bg");
+
+                    $("#nav-transparent #main_logo").css("display", "inline-block");
+
+                    $("#nav-transparent #light_logo").css("display", "none");
+
+                } else {
+
+                    $(".navigation-fixed-wrapper").removeClass("nav-white-bg");
+
+                    $("#nav-transparent #light_logo").css("display", "inline-block");
+
+                    $("#nav-transparent #main_logo").css("display", "none");
+
+                }
+
+            }
+
+        })
+
+    }
+
+
+
+
+
+
+
+    /*---------------------
+
+    Overlay Nav
+
+    -----------------------*/
+
+    $("#navigation").navigation({
+
+        offset: 20,
+
+        overlayColor: "rgba(0,0,0,0.6)",
+
+        effect: "slide"
+
+    });
+
+
+
+    /*---------------------
+
+    Affix Nav
+
+    -----------------------*/
+
+    $("#navigation").fixed({
+
+        offset: 20
+
+    });
+
+
+
+    /*---------------------
+
+    Hidden Nav
+
+    -----------------------*/
+
+
+
+    if ($("#navigation-push").length !== 0) {
+
+        if ($(window).width() > 991) {
+
+            $("#navigation-push").find($(".nav-menus-wrapper").addClass("nav-menus-wrapper-open"));
+
+            $("#navigation-push").find($(".nav-menus-wrapper-close-button").hide());
+
+            $("#navigation-push").find($(".small-size-header").hide());
+
+        } else {
+
+            $("#main_logo").css("display", "none");
+
+            $("#navigation-push").find($(".nav-menus-wrapper").removeClass("nav-menus-wrapper-open"));
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+    /*------------------------------------
+
+        8. Scroll To Top
+
+    --------------------------------------*/
+
+
+
+    $(window).scroll(function() {
+
+        if ($(this).scrollTop() > 500) {
+
+            $(".scroll-to-top").fadeIn(400);
+
+
+
+        } else {
+
+            $(".scroll-to-top").fadeOut(400);
+
+        }
+
+    });
+
+
+
+    $(".scroll-to-top").on('click', function(event) {
+
+        event.preventDefault();
+
+        $("html, body").animate({
+
+            scrollTop: 0
+
+        }, 600);
+
+    });
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(window).scroll(function() {    
+
+    var scroll = $(window).scrollTop();
+
+
+
+    if (scroll >= 300) {
+
+        $(".navigation-fixed-wrapper").addClass("PinkHeader");
+
+    } else {
+
+        $(".navigation-fixed-wrapper").removeClass("PinkHeader");
+
+    }
+
+});
+
+
+
+
+
+
+
---
+++ js/sliders_script.js
@@ -0,0 +1,141 @@
+$.noConflict();
+
+jQuery( document ).ready(function( $ ) {
+
+    'use strict'
+
+    //CheckPositionMobile();
+
+    $('.tp-banner').show().revolution({
+
+
+        dottedOverlay: "none",
+
+        delay: 3500,
+
+        startwidth: 1170,
+
+        startheight: 960,
+
+        hideThumbs: 200,
+
+        thumbWidth: 100,
+
+        thumbHeight: 50,
+
+        thumbAmount: 5,
+
+        navigationType: "none",
+
+        navigationArrows: "hide",
+
+        navigationStyle: "none",
+
+        navigationInGrid: "off",
+
+        touchenabled: "on",
+
+        onHoverStop: "off",
+
+        swipe_velocity: 0.7,
+
+        swipe_min_touches: 1,
+
+        swipe_max_touches: 1,
+
+        drag_block_vertical: false,
+
+        parallax: "mouse",
+
+        parallaxBgFreeze: "off",
+
+        parallaxLevels: [7, 4, 3, 2, 5, 4, 3, 2, 1, 0],
+
+        keyboardNavigation: "off",
+
+        navigationHAlign: "center",
+
+        navigationVAlign: "bottom",
+
+        navigationHOffset: 0,
+
+        navigationVOffset: 20,
+
+        soloArrowLeftHalign: "left",
+
+        soloArrowLeftValign: "center",
+
+        soloArrowLeftHOffset: 20,
+
+        soloArrowLeftVOffset: 0,
+
+        soloArrowRightHalign: "right",
+
+        soloArrowRightValign: "center",
+
+        soloArrowRightHOffset: 20,
+
+        soloArrowRightVOffset: 0,
+
+        shadow: 0,
+
+        fullWidth: "off",
+
+		fullScreenAlignForce:"off",
+		
+        fullScreen: "off",
+
+        spinner: "spinner4",
+
+        stopLoop: "off",
+
+        stopAfterLoops: -1,
+
+        stopAtSlide: -1,
+
+        shuffle: "off",
+
+        autoHeight: "off",
+
+        forceFullWidth: "off",
+
+        hideThumbsOnMobile: "off",
+
+        hideNavDelayOnMobile: 1500,
+
+        hideBulletsOnMobile: "off",
+
+        hideArrowsOnMobile: "off",
+
+        hideThumbsUnderResolution: 0,
+
+        hideSliderAtLimit: 0,
+
+        hideCaptionAtLimit: 0,
+
+        hideAllCaptionAtLilmit: 0,
+
+        startWithSlide: 0,
+
+        //fullScreenOffsetContainer: "header"
+
+    });
+
+	
+
+	
+
+	
+
+	
+
+	
+
+    
+
+	
+
+	
+
+})(jQuery);
+
